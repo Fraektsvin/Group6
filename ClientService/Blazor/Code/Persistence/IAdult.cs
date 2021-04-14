d95ea5bad522973a9f8beaeb4bd2c5dbdbbc3ddc
Antonio <32570073+Fraektsvin@users.noreply.github.com>
2021-04-14 13:51:53 +0200

    ClientArchitecture
    Architecture for the client part - will be modified later

---
+++ ClientService/Blazor/Code/Persistence/IAdult.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace Blazor.code.persistence 
+{
+    public interface IAdult
+    {
+        void SaveChanges(Person addAdult);
+        IList<T> ReadData<T>(string s);
+        void RemoveAdult(int adultId);
+        Adult get(int id);
+        void Update(Adult adult);
+        IList<Adult> getAdults();
+        void addData(Adult adult); 
+    }
+}
