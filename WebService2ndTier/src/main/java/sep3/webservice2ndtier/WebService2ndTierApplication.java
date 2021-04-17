9142e6a8717aa6f44e13d9bff7196270c241b5f7
ConstantinaTripon <44843810+Nina8507@users.noreply.github.com>
2021-04-17 11:37:23 +0200

    Creating a spring boot project for second tier

---
+++ WebService2ndTier/src/main/java/sep3/webservice2ndtier/WebService2ndTierApplication.java
@@ -0,0 +1,13 @@
+package sep3.webservice2ndtier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService2ndTierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebService2ndTierApplication.class, args);
+    }
+
+}
---
+++ WebService2ndTier/src/test/java/sep3/webservice2ndtier/WebService2ndTierApplicationTests.java
@@ -0,0 +1,13 @@
+package sep3.webservice2ndtier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebService2ndTierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
